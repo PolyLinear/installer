621fa6172377f1cd3fca3f928b7bcc3bec94bd9c
PolyLinear <bciesla@umass.edu>
2023-12-25 03:39:06 -0500

    basic mounting + fstab

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@
 #assuming internet is connected
 
 #verify boot mode, assure 64-bit x64 UEFI
-#[[ $(cat /sys/firmware/efi/fw_platform_size) -eq 64 ]] || {
-#	printf "%s\n" "Please assure that the system is using a 64-bit x64 UEFI" "Set up will not continue otherwise"
-#	exit 1
-#}
-printf "%s\n\n" "Passed 64-it x64 UEFI check"
+[[ $(cat /sys/firmware/efi/fw_platform_size) -eq 64 ]] || {
+	printf "%s\n" "Please assure that the system is using a 64-bit x64 UEFI" \
+	"Set up will not continue otherwise"
+	exit 1
+}
+
+printf "%s\n\n" "Passed 64-bit x64 UEFI check"
 
 mapfile -t devices < <(lsblk -dpno name)
 
@@ -18,7 +20,8 @@ PS3="Select device to partition: "
 echo "Devices:"
 select device in "${devices[@]}"; do
 	if [[ -n $device ]]; then
-		printf "%s\n" "Are you sure you want to install onto ${device}?" "This will wipe the device completely [y\n]"
+		printf "%s\n" "Are you sure you want to install onto ${device}?" \
+		    "This will wipe the device completely [y\n]"
 		read -r decision
 		if [[ $decision != "y" ]]; then
 			continue
@@ -29,10 +32,25 @@ select device in "${devices[@]}"; do
 
 done
 
-parted --script "$device_to_install" \
+parted -f --script "$device_to_install" \
     mklabel gpt \
     mkpart '"EFI Partition"' fat32 1MiB 1GiB \
     set 1 esp on \
     mkpart '"Swap Partition"' linux-swap 1GiB 5GiB \
     mkpart '"Root Partition"' ext4 5GiB 100%
+
+mkfs.fat -F 32 "${device_to_install}1"
+
+mkfs.swap "${device_to_install}2"
+swapon "${device_to_install}2"
+
+mkfs.ext4 "${device_to_install}3"
+
+mount "${device_to_install}3" /mnt
+mount --mkdir "${device_to_install}1" /mnt/boot
+
+pacstrap -K /mnt base linux linux-firmware neovim
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
 
