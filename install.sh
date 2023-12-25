e7bca5f7c1b77e434382c517ee8c4708346e4fa8
PolyLinear <bciesla@umass.edu>
2023-12-25 00:46:26 -0500

    prompt user for device

--- install.sh
+++ install.sh
@@ -4,24 +4,28 @@
 #assuming internet is connected
 
 #verify boot mode, assure 64-bit x64 UEFI
-[[ $(cat /sys/firmware/efi/fw_platform_size) -eq 64 ]] || {
-	printf "%s\n" "Please assure that the system is using a 64-bit x64 UEFI" "Set up will not continue otherwise"
-	exit 1
-}
+#[[ $(cat /sys/firmware/efi/fw_platform_size) -eq 64 ]] || {
+#	printf "%s\n" "Please assure that the system is using a 64-bit x64 UEFI" "Set up will not continue otherwise"
+#	exit 1
+#}
 printf "%s\n\n" "Passed 64-it x64 UEFI check"
 
 mapfile -t devices < <(lsblk -dpno name)
 
-declare device_to_install 
+declare device_to_install
 
 PS3="Select device to partition: "
 echo "Devices:"
-select device in "${devices[@]}";
-do
-    if [[ -n $device ]]; then
-	device_to_install=$device
-	break
-    fi
+select device in "${devices[@]}"; do
+	if [[ -n $device ]]; then
+		printf "%s\n" "Are you sure you want to install onto ${device}?" "This will wipe the device completely [y\n]"
+		read -r decision
+		if [[ $decision != "y" ]]; then
+			continue
+		fi
+		device_to_install=$device
+		break
+	fi
 
 done
 
