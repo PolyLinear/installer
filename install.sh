3f428c46f5c7197e45c4f4b980a012ee8075fa27
PolyLinear <bciesla@umass.edu>
2024-01-02 03:58:15 -0500

    added comments + fixed bug for nvme

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 #assuming internet is connected
 language="en_US.UTF-8"
 hostname="archlinux"
-export username="user"
-export dot_files="https://github.com/PolyLinear/dotfiles"
+username="user"
+dot_files="https://github.com/PolyLinear/dotfiles"
 partition() {
 
+	#check if running on 64bit efi. (install won't work otherise)
 	[[ $(cat /sys/firmware/efi/fw_platform_size) -eq 64 ]] || {
 		printf "%s\n" "Please assure that the system is using a 64-bit x64 UEFI" \
 			"Set up will not continue otherwise"
@@ -16,8 +17,10 @@ partition() {
 
 	printf "%s\n\n" "Passed 64-bit x64 UEFI check"
 
+	#build selection of available block devices
 	mapfile -t devices < <(lsblk -dpno name)
 
+	#select device to install arch linux to
 	PS3="Select device to partition: "
 	echo "Devices:"
 	select device in "${devices[@]}"; do
@@ -33,24 +36,49 @@ partition() {
 		fi
 
 	done
+
+	#nvme drives don't follow the naming scheme as other sda or vda devices
+	#need to add "p" before partition number if nvme is used
+	if [[ "$device_to_install" =~ .*nvme.* ]]; then
+		boot=p1
+		root=p2
+	else
+		boot=1
+		root=2
+	fi
+
+	#partitions of the system
+	export boot_part="${device_to_install}${boot}"
+	export root_part="${device_to_install}${root}"
+
+	#create a 1GB boot directory. The rest of the space is dedicated to root and swap
 	sgdisk --zap-all "${device_to_install}"
 	sgdisk --clear \
 		--new=1:0:+1G --typecode=1:ef00 --change-name=1:"boot" \
 		--new=2:0:0 --typecode=2:8309 --change-name=2:"cryptlvm" "${device_to_install}"
 
-	cryptsetup luksFormat -s 512 "${device_to_install}2"
-	cryptsetup open "${device_to_install}2" cryptlvm
+	#setup LVM on LUKS
+	cryptsetup luksFormat -s 512 "${root_part}"
+	cryptsetup open "${root_part}" cryptlvm
+
+	#physical volume
 	pvcreate /dev/mapper/cryptlvm
+
+	#volume group
 	vgcreate vgsystem /dev/mapper/cryptlvm
+
+	#first 4G are for swap, rest is for root
 	lvcreate -L 4G vgsystem -n swap
 	lvcreate -l '100%FREE' vgsystem -n root
 
+	#format logical volumes and enable swap
 	mkfs.ext4 /dev/vgsystem/root
 	mkswap /dev/vgsystem/swap
-	mkfs.fat -F 32 "${device_to_install}1"
+	mkfs.fat -F 32 "${boot_part}"
 
+	#mount logical volumes and start swap
 	mount /dev/vgsystem/root /mnt
-	mount --mkdir "${device_to_install}1" /mnt/boot
+	mount --mkdir "${boot_part}" /mnt/boot
 	swapon /dev/vgsystem/swap
 
 }
@@ -69,15 +97,19 @@ function installation() {
 
 #TODO set time sync
 function locale_and_time() {
+	#New York Time Zone
 	ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 	hwclock --systohc
 
+	#set language
 	echo "LANG=${language}" >/etc/locale.conf
 	sed -i "/${language}/s/^#//" /etc/locale.gen
 	locale-gen
 
+	#set hostname
 	echo "${hostname}" >/etc/hostname
 
+	#enable time syncing
 	systemctl enable systemd-timesyncd.service
 
 }
@@ -102,13 +134,12 @@ function bootloader() {
 	systemd-machine-id-setup
 	bootctl --path=/boot install
 
-	#initrd /intel-ucode.img
 	#create bootloader entry
 	cat <<EOF >/boot/loader/entries/arch.conf
 title   Arch
 linux   /vmlinuz-linux $([[ -n $ucode ]] && printf "\n%s\n" "initrd  /${ucode}.img")
 initrd  /initramfs-linux.img
-options cryptdevice=UUID=$(blkid -s UUID -o value "${device_to_install}2"):cryptlvm root=/dev/vgsystem/root rw
+options cryptdevice=UUID=$(blkid -s UUID -o value "${root_part}"):cryptlvm root=/dev/vgsystem/root rw
 EOF
 
 	#configuration information
@@ -134,13 +165,13 @@ function pacman_setup() {
 	reflector --latest 25 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 }
 
-#TODO: setup sudoers file, enable wifi and firewall support
 function base() {
 
 	#create initial user
 	useradd -m -U $username
 	passwd $username
 
+	#read user defined packages and install them
 	pacinstall --no-confirm --resolve-replacements=provided --resolve-conflicts=provided $(awk '/^[^\[]/ {print $1}' /packages.txt)
 
 	#enable necessary daemons
@@ -157,10 +188,12 @@ function base() {
 	usermod -a -G wheel,libvirt $username
 }
 
-#TODO fetch dot files from repo and apply
 function user_specific_configurations() {
+
+	#fetch dotfiles from github
 	git clone "$dot_files" ~/.dotfiles
 
+	#setup bash environment
 	ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 	ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile && source ~/.bash_profile
 
@@ -168,37 +201,55 @@ function user_specific_configurations() {
 		[[ -d "$1" ]] || mkdir -p "$1"
 	}
 
+	#basic XDG defined in .bash_profile. create directories if needed
 	define_XDG "$XDG_DATA_HOME"
 	define_XDG "$XDG_STATE_HOME"
 	define_XDG "$XDG_CACHE_HOME"
 	define_XDG "$XDG_CONFIG_HOME"
 
+	#link config files dotfiles
 	for file in ~/.dotfiles/config/*; do
 		ln -s "$file" "$XDG_CONFIG_HOME/$(basename "$file")"
 	done
 
+	#directory for user defined scripts
 	ln -s ~/.dotfiles/scripts ~/scripts
 
-	#code taken from vim-plug github repo
+	#code taken from vim-plug github repo, install vim-plug
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+	#install plugins
 	nvim +PlugInstall +qall
+
+	#enable mpd for music
 	systemctl --user enable mpd.service
+
+	#set firefox for default browser
 	xdg-settings set default-web-browser firefox.desktop
+
+	#create base xdg directories
 	xdg-user-dirs-update --force
 }
 
 function configure() {
+
+	#need to directory to enable systemctl functionaity for user
 	mkdir /run/user/$(id -u "$username")
+
+	#set defaults for user
 	export -f user_specific_configurations
 	su $username -c "user_specific_configurations"
+
+	#disable trackpad from custom udev rule
 	cp /home/$username/.dotfiles/99-myfavoritetrackpoint.rules /etc/udev/rules.d/
 }
 
 function cleanup() {
 	rm /mnt/install.sh /mnt/packages.txt
-	umount -qR /mnt
+	umount /mnt/boot
+	umount /mnt
+	swapoff /dev/mapper/swap
 }
 
 if [[ "$1" = "setup" ]]; then
